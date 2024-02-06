6fee5714214aee53462572469b7dbeb6e07819f3
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-06 18:37:37 -0500

    add price component

---
+++ src/app/products/components/price/price.component.ts
@@ -0,0 +1,10 @@
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'products-price',
+  templateUrl: './price.component.html',
+  styleUrls: ['./price.component.css']
+})
+export class PriceComponent {
+
+}

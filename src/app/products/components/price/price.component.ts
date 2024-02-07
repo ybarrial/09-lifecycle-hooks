e3d8d0a57f7b69501fe388051853f605600fcade
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-06 19:50:47 -0500

    implements onInit, onDestroy, OnChange

--- src/app/products/components/price/price.component.ts
+++ src/app/products/components/price/price.component.ts
@@ -1,10 +1,28 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'products-price',
   templateUrl: './price.component.html',
   styleUrls: ['./price.component.css']
 })
-export class PriceComponent {
+export class PriceComponent implements OnInit, OnChanges, OnDestroy {
+
+  @Input()
+  public price: number = 0;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log('Componente HIJO: ngOnInit');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Componente HIJO: ngOnChanges');
+    console.log({ changes });
+  }
+
+  ngOnDestroy(): void {
+    console.log('Componente HIJO: ngOnDestroy');
+  }
 
 }
